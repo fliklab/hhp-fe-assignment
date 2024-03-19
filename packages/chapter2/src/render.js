7d63db7230067b285f6463cc82adb7bd1e074130
Flik Jeong <jsh852@gmail.com>
2024-03-19 21:41:58 +0900

    Pass test-a-1

--- packages/chapter2/src/render.js
+++ packages/chapter2/src/render.js
@@ -1,9 +1,23 @@
+// jsx("div", null, "div의 children 입니다.");
+//
+// {
+//   div: {
+//     props: null,
+//     children: ["div의 children 입니다."],
+//   },
+// }
 export function jsx(type, props, ...children) {
-  return {}
+  const result = {};
+  result[type] = {
+    props,
+    children,
+  };
+  return result;
 }
 
 export function createElement(node) {
   // jsx를 dom으로 변환
+  return {};
 }
 
 function updateAttributes(target, newProps, oldProps) {
@@ -12,7 +26,6 @@ function updateAttributes(target, newProps, oldProps) {
   //     다음 속성으로 넘어감 (변경 불필요)
   //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
   //     target에 해당 속성을 새 값으로 설정
-
   // oldProps을 반복하여 각 속성 확인
   //   만약 newProps들에 해당 속성이 존재한다면
   //     다음 속성으로 넘어감 (속성 유지 필요)
@@ -20,25 +33,25 @@ function updateAttributes(target, newProps, oldProps) {
   //     target에서 해당 속성을 제거
 }
 
+// render($root, jsx("div", null, "div의 children 입니다.")) 했을때
+// expect($root.innerHTML).toBe(`<div>div의 children 입니다.</div>`);
 export function render(parent, newNode, oldNode, index = 0) {
+  const newNodeElement = document.createElement(Object.keys(newNode)[0]);
+  newNodeElement.innerHTML = newNode[Object.keys(newNode)[0]].children[0];
+  parent.appendChild(newNodeElement);
   // 1. 만약 newNode가 없고 oldNode만 있다면
   //   parent에서 oldNode를 제거
   //   종료
-
   // 2. 만약 newNode가 있고 oldNode가 없다면
   //   newNode를 생성하여 parent에 추가
   //   종료
-
   // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
   //   oldNode를 newNode로 교체
   //   종료
-
   // 4. 만약 newNode와 oldNode의 타입이 다르다면
   //   oldNode를 newNode로 교체
   //   종료
-
   // 5. newNode와 oldNode에 대해 updateAttributes 실행
-
   // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
   //   각 자식노드에 대해 재귀적으로 render 함수 호출
 }
