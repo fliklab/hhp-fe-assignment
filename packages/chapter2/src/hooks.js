49cc3da3dadd912a2541363b4ce6141bb75257a3
Flik Jeong <jsh852@gmail.com>
2024-03-22 19:50:28 +0900

    Pass test 1-3 : define useState hook and add setState function

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,19 @@
 export function createHooks(callback) {
-  const useState = (initState) => {
-    return [];
+  const useState = initState => {
+    let state = initState;
+    const setState = newState => {
+      if (state === newState) return;
+      callback();
+      state = newState;
+    };
+    return [state, setState];
   };
 
   const useMemo = (fn, refs) => {
     return fn();
   };
 
-  const resetContext = () => {
-
-  }
+  const resetContext = () => {};
 
   return { useState, useMemo, resetContext };
 }
